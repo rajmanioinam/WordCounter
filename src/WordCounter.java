362b0f799ee1fa7dce7bfb89947886b8dff3aca6
rajmanioinam <31492859+rajmanioinam@users.noreply.github.com>
2017-11-12 22:14:01 -0700

    Code change

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -2,7 +2,8 @@ import java.io.File;
 import java.util.Scanner;
 
 public class WordCounter {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws Exception{
+        File file = new File("ataleoftwocities.txt");
+        Scanner scanner = new Scanner(file);
     }
 }
