c688dd8e9aef288db1cf198ee4ae8e341f0afaac
rajmanioinam <31492859+rajmanioinam@users.noreply.github.com>
2017-11-13 10:17:56 -0700

    Testing Cloning

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class WordCounter {
     public static void main(String[] args) throws Exception{
+        System.out.println("testing in new machine");
         File file = new File("ataleoftwocities.txt");
         Scanner scanner = new Scanner(file);
     }
