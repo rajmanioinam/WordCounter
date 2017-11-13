761a4d549365c9fcdf56c2f170887323bed35838
rajmanioinam <31492859+rajmanioinam@users.noreply.github.com>
2017-11-13 11:04:00 -0700

    Added: Word counting routine

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 
 public class WordCounter {
     public static void main(String[] args) throws Exception{
-        System.out.println("testing in new machine");
+        System.out.println("This is a word counter routine.");
         File file = new File("ataleoftwocities.txt");
         Scanner scanner = new Scanner(file);
+        int wordCount = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            wordCount += line.split("").length;
+        }
+        System.out.println("The file contains " + wordCount + " word(s).");
     }
 }
